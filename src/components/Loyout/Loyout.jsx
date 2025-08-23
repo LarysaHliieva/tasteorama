820eada15dfb433f969eeaf71f78468c3e80deb0
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-24 00:50:24 +0300

    add loyout style

--- src/components/Loyout/Loyout.jsx
+++ src/components/Loyout/Loyout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 
+import css from "./Loyout.module.css";
+
 export default function Loyout() {
   return (
-    <>
+    <div className={css.loyout}>
       Header
-      <Outlet />
+      <div className={css.loyoutOutlet}>
+        <Outlet />
+      </div>
       Footer
-    </>
+    </div>
   );
 }
