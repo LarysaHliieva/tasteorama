0196d2ba9a3e305b4e07319416c6c328dac85794
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-25 23:54:07 +0300

    add profilePage

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import css from "./Profile.module.css";
+import clsx from "clsx";
+
+export default function Profile() {
+  return (
+    <div className={clsx(css.profile, "container")}>
+      <div className={css.header}>
+        <h2 className={css.title}>My profile</h2>
+        <nav className={css.nav}>
+          <NavLink to="own" className={css.navItem}>
+            My Recipes
+          </NavLink>
+          <NavLink to="favorites" className={css.navItem}>
+            Saved Recipes
+          </NavLink>
+        </nav>
+        <div className={css.counter}>96 recipes</div>
+      </div>
+    </div>
+  );
+}
