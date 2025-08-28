bea145d5d183974b41f8062c181067e6b73063b0
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-28 23:54:55 +0300

    fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import { FilterSelect } from "../../components/filterComponent/filterComponent";
 import { RecipeList } from "../../components/RecipeList/RecipeList";
-import RecipeCard from "../../components/RecipeCard/RecipeCard";
-
-import { recipes } from "../../utils/recipes";
 
 export default function MainPage() {
   return (
@@ -12,9 +9,6 @@ export default function MainPage() {
       <Hero />
       <FilterSelect />
       <RecipeList />
-      <RecipeCard {...recipes[3]} />
-      <RecipeCard {...recipes[3]} variant="details" />
-      <RecipeCard {...recipes[3]} variant="favorites" />
     </div>
   );
 }
