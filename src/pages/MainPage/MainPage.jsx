5d788be9a9593b94c0a9b1954aee49e80d701c50
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-28 06:22:37 +0300

    add style modal

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "../../components/Hero/Hero";
 import { FilterSelect } from "../../components/filterComponent/filterComponent";
 import { RecipeList } from "../../components/RecipeList/RecipeList";
 
+import Modal from "../../components/Modal/Modal";
+
 export default function MainPage() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <Hero />
       <FilterSelect />
       <RecipeList />
+
+      <button onClick={() => setIsOpen(true)}>Відкрити модалку</button>
+
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          title="Error while saving"
+          desc="To save this recipe, you need to thorize first"
+          confirmText="Log in"
+          cancelText="Register"
+          type="navigate"
+        />
+      )}
     </div>
   );
 }
