57809f4930a11a5e7402a4a0b5540fc31f8d8335
KyykMaks <139263385+KyykMaks@users.noreply.github.com>
2025-08-29 18:26:47 +0300

    Filter categories/ingredients

---
+++ src/components/containers/recipesContainer.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRecipes } from "../../redux/recipes/operations";
+import {
+  selectRecipes,
+  selectRecipesPage,
+  selectRecipesTotalPages,
+} from "../../redux/recipes/selectors";
+import {
+  selectFiltersCategories,
+  selectFiltersIngredients,
+  selectSearchQuery,
+} from "../../redux/filters/selectors";
+import { RecipeList } from "../RecipeList/RecipeList";
+
+
+export function RecipeContainer() {
+  const dispatch = useDispatch();
+  const all = useSelector(selectRecipes);
+  const page = useSelector(selectRecipesPage);
+  const totalPages = useSelector(selectRecipesTotalPages);
+
+  const categories = useSelector(selectFiltersCategories);
+  const ingredients = useSelector(selectFiltersIngredients);
+  const searchQuery = useSelector(selectSearchQuery);
+
+  const filters = { categories, ingredients, searchQuery };
+
+  useEffect(() => {
+    dispatch(getRecipes({ page: 1, limit: 12, filters }));
+  }, [categories, ingredients, searchQuery, dispatch]);
+
+  const loadMore = () => {
+    dispatch(getRecipes({ page: page + 1, limit: 12, filters }));
+  };
+
+  return (
+    <RecipeList
+      recipes={all}
+      page={page}
+      totalPages={totalPages}
+      onLoadMore={loadMore}
+    />
+  );
+}
