8ea107ff41bb88339fc997ee6fcc71448c0d8ccc
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-29 16:04:19 +0300

    merge main

--- src/services/api.js
+++ src/services/api.js
@@ -5,28 +5,12 @@ const axiosAPI = axios.create({
   withCredentials: true,
 });
 
-export const createRecipe = async (recipe) => {
-  const formData = new FormData();
-  Object.keys(recipe).forEach((key) => {
-    if (key === "ingredients") {
-      formData.append(key, JSON.stringify(recipe[key]));
-    } else {
-      formData.append(key, recipe[key]);
-    }
-  });
-
-  const { data } = await axios.post(API_URL, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
-  return data;
-};
-
 axiosAPI.interceptors.request.use((config) => {
-    const token = localStorage.getItem("accessToken")
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-})
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default axiosAPI;
