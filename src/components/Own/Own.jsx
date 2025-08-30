7e843e90eff02f3a88ad6cb46c996fccdb9c38b8
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-31 00:51:42 +0300

    fix

--- src/components/Own/Own.jsx
+++ src/components/Own/Own.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { RecipeList } from "../RecipeList/RecipeList";
+
+import { selectFavorites } from "../../redux/recipes/selectors";
+import { getFavorites } from "../../redux/recipes/operations";
+
+import css from "./Own.module.css";
 
 export default function Own() {
-  return <div>Own</div>;
+  const dispatch = useDispatch();
+  const favorites = useSelector(selectFavorites);
+  // const page = useSelector(selectRecipesPage);
+  // const totalPages = useSelector(selectRecipesTotalPages);
+
+  useEffect(() => {
+    dispatch(getFavorites());
+  }, [dispatch]);
+
+  const loadMore = () => {
+    // dispatch(getFavourite({ page: page + 1, limit: 12 }));
+  };
+
+  return (
+    <div>
+      <div className={css.counter}>96 recipes</div>
+      <RecipeList
+        recipes={favorites}
+        // page={page}
+        // totalPages={totalPages}
+        onLoadMore={loadMore}
+        variant="favorites"
+      />
+    </div>
+  );
 }
