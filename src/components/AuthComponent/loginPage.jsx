3278d719e1d2438dfae1f274c9d05c66844eaf4b
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-30 20:56:16 +0300

    fix

--- src/components/AuthComponent/loginPage.jsx
+++ src/components/AuthComponent/loginPage.jsx
@@ -6,56 +6,57 @@ import { useDispatch } from "react-redux";
 import { login } from "../../redux/auth/slice.js";
 
 export default function LoginPage() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState("")
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const handleLogin = async (e) => {
-    e.preventDefault()
+  const handleLogin = async (e) => {
+    e.preventDefault();
     try {
-        const res = await AuthAPI.login({ email, password })
-        localStorage.setItem("accessToken", res.data.accessToken)
-        localStorage.setItem("refreshToken", res.data.refreshToken)
-        localStorage.setItem("user", JSON.stringify(res.data.data))
-        dispatch(login(res.data.data))
-        navigate('/')
+      const res = await AuthAPI.login({ email, password });
+      console.log({ res });
+      localStorage.setItem("accessToken", res.data.data.accessToken);
+      localStorage.setItem("refreshToken", res.data.data.refreshToken);
+      localStorage.setItem("user", JSON.stringify(res.data.data));
+      dispatch(login(res.data.data));
+      navigate("/");
     } catch (err) {
-            if (err.response) {
-            const messages = err.response.data.errors || [err.response.data.message]
-            setError(messages.join(", "))
-        
-        }
+      if (err.response) {
+        const messages = err.response.data.errors || [
+          err.response.data.message,
+        ];
+        setError(messages.join(", "));
+      }
     }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <label>Enter your email address</label>
+        <input
+          type="text"
+          placeholder="email@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Enter your password</label>
+        <input
+          type="password"
+          placeholder="**********"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don`t have an account?
+        <Link to={"/auth/register"}>Register</Link>
+      </p>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
 }
-
-    return (
-        <div>
-            <form onSubmit={handleLogin}>
-                <h1>Login</h1>
-                <label>Enter your email address</label>
-                <input
-                    type="text"
-                    placeholder="email@gmail.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label>Enter your password</label>
-                <input
-                    type="password"
-                    placeholder="**********"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
-            <p>Don`t have an account?
-                <Link to={"/auth/register"}>Register</Link>
-            </p>
-                        {error && <p style={{color: "red"}}>{error}</p>}
-        </div>
-    )
-};
-
