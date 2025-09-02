ea738ec62c5947b95641737911e6231e9bec3f13
KyykMaks <139263385+KyykMaks@users.noreply.github.com>
2025-09-02 21:15:31 +0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosAPI = axios.create({
   baseURL: "https://group2-b.onrender.com/", //сюди потрібно додати посилання на наш бекенд
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 axiosAPI.interceptors.request.use((config) => {
